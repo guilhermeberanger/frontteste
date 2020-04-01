15ffaafa16e7a160b2f77af2494d4175ee1d6998
guilhermeberanger <guilherme.sberanger@gmail.com>
2020-04-01 17:00:47 -0300

    fetch api v3

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const dataForm = document.getElementById("myForm");
     dataForm.addEventListener("submit", function (event) {
         event.preventDefault()
-
-        const cepOrigem = document.getElementById("cepOrigemID").value;
+        //-------------------------------
+        const cepOrigem = document.getElementById("cepOrigemID").value; // você vai pegar as informções do quadrado CEP ORIGEM
         const cepDestino = document.getElementById("cepDestinoID").value;
         const tipoCarga = document.getElementById("tipoCargaId").value;
         const valorDeclarado = document.getElementById("valorDeclaradoId").value;
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const altura = document.getElementById("alturaId").value;
         const largura = document.getElementById("larguraId").value;
         const peso = document.getElementById("pesoId").value;
-
+        ;
         const objeto = {
             cepOrigem,
             cepDestino,
@@ -28,20 +28,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
 
-        const url = 'http://localhost:9000/.netlify/functions/braspres'
+        //----------------------
+        const urls = [
+            'https://sad-babbage-d06970.netlify.com/.netlify/functions/braspres',
+            'https://sad-babbage-d06970.netlify.com/.netlify/functions/correios',
+            'https://sad-babbage-d06970.netlify.com/.netlify/functions/centralfrete',
 
-        fetch('https://adoring-lewin-4ef470.netlify.com/.netlify/functions/braspres', {
-            method: 'POST',
-            headers: new Headers({
-                'access-control-allow-origin': '*',
-                'Access-Control-Allow-Headers': '*' 
-            }),
-            body: JSON.stringify(objeto)
-        })
-            .then(response => response.json())
-            //.catch(error => console.error('Error:', error))
-            .then(response => console.log('Success:', JSON.stringify(response)))
-            .catch(error => console.error('Error:', error))
+        ];
+
+        // use map() to perform a fetch and handle the response for each url
+        Promise.all(urls.map(url =>
+            fetch(url, {
+                method: 'POST',
+                headers: new Headers({
+                    'access-control-allow-origin': '*',
+                    'Access-Control-Allow-Headers': '*'
+                }),
+                body: JSON.stringify(objeto)
+            })
+                .then(response => response.json())
+                //.catch(error => console.error('Error:', error))
+                .then(response => console.log('Success:', JSON.stringify(response)))
+                //.then(response => console.log(
+                      //  response.CALCULOFRETE,
+                        //response.sender,
+                       // response.cServico
+                  //  ))
+                .catch(error => console.error('Error:', error))
+        ))
+        //--------------------------------------------
 
 
     });
